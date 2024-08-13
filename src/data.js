ea1b5d9149bc187fbd7e081129203f09033aadf6
Satwik Kumar <satwikkr393@gmail.com>
2024-08-13 17:05:27 +0530

    slider added

---
+++ src/data.js
@@ -0,0 +1,24 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://m.media-amazon.com/images/I/71FPhIXHGoL._SX679_.jpg",
+    title: "ON HIGH DEMAND",
+    desc: "HIGH QUALITY WATCHES",
+    bg: "#F8F5F2",
+  },
+  {
+    id: 2,
+    img: "https://m.media-amazon.com/images/I/71qvw3zE49L._SY741_.jpg",
+    title: "SUMMER SALE",
+    desc: "FLAT 30% OFF ON NEW ARRIVALS",
+    bg: "#FFFBF8",
+  },
+
+  {
+    id: 3,
+    img: "https://m.media-amazon.com/images/I/61iKZtIs73L._SY695_.jpg",
+    title: "POPULAR",
+    desc: "FREE SHIPPING ON POPULAR FOOTWEARS",
+    bg: "#F5F5F3",
+  },
+];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Announcements from "../components/Announcements";
+import Slider from "../components/Slider";
 
 const Home = () => {
   return (
     <div>
       <Announcements />
       <Navbar />
+      <Slider />
     </div>
   );
 };
