007081ee637ba661efbe10ff51cd963030d29376
Satwik Kumar <satwikkr393@gmail.com>
2024-11-18 00:24:06 +0530

    added other imp routes

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,15 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
 
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cors());
 
 dotenv.config({ path: "./.env" });
 
@@ -26,6 +32,10 @@ mongoose
 
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/user", userRoute);
+app.use("/api/v1/product", productRoute);
+app.use("/api/v1/cart", cartRoute);
+app.use("/api/v1/order", orderRoute);
+app.use("/api/v1/stripe", stripeRoute);
 
 //Listenting to server
 app.listen(process.env.PORT || 3000, (req, res) => {
